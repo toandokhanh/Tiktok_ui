0faba4b5f4824838cb1680e0a3315c58285f3575
Toan vip pro <toanb2012046@student.ctu.edu.vn>
2022-08-04 18:07:20 +0700

    f8

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Content from '~/component/content';
+
+function App() {
+    return (
+        <div className="App">
+            <Content />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/component/content/index.js
@@ -0,0 +1,16 @@
+import '../content/content.css';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Content = (props) => {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
+};
+
+Content.propTypes = {};
+
+export default Content;
